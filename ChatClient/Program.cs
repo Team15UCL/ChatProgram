e0875684811d7a0dda9a115d7995650493007f01
Aske Lysgaard <askelysgaard@gmail.com>
2024-03-07 14:35:41 +0100

    upload programmet

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,9 +1,66 @@
-﻿namespace ChatClient;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ExecuteClient();
+    }
+
+    static void ExecuteClient()
+    {
+        try
+        {
+            // Finder Ip-addressen til den lokale computer
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            // Vælger port vi vil bruge
+            IPEndPoint localEndPoint = new(ipAddr, 65000);
+
+            // Laver en socket ud fra vores ip og port
+            Socket clientSocket = new(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(localEndPoint);
+
+                Console.WriteLine($"Socket connected to: {clientSocket.RemoteEndPoint}");
+
+                byte[] messageSent = Encoding.ASCII.GetBytes("Hej fra klienten");
+                int byteSent = clientSocket.Send(messageSent);
+
+                byte[] messageReceived = new byte[1024];
+
+                int byteReceived = clientSocket.Receive(messageReceived);
+
+                Console.WriteLine($"Besked fra server: {Encoding.ASCII.GetString(messageReceived)}");
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine($"ANexception: {ane}");
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"SException: {se}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e}");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,9 +1,65 @@
-﻿namespace ChatServer;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ExecuteServer();
+    }
+
+    public static void ExecuteServer()
+    {
+        try
+        {
+            // Finder Ip-addressen til den lokale computer
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            // Vælger port vi vil bruge
+            IPEndPoint localEndPoint = new(ipAddr, 65000);
+
+            // Laver en socket ud fra vores ip og port
+            Socket serverSocket = new(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                serverSocket.Bind(localEndPoint);
+
+                serverSocket.Listen(10);
+
+                Console.WriteLine("Waiting for connection...");
+
+                Socket clientSocket = serverSocket.Accept();
+
+                // buffer til at modtage
+                byte[] bytes = new byte[1024];
+                string data = null;
+
+                int numByte = clientSocket.Receive(bytes);
+
+                data += Encoding.ASCII.GetString(bytes, 0, numByte);
+
+                Console.WriteLine($"Modtaget besked: {data}");
+
+                byte[] message = Encoding.ASCII.GetBytes("Hej fra serveren");
+
+                clientSocket.Send(message);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
     }
 }
